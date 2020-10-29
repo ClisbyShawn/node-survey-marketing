2014abf1b5f4556786c5a83c0384ca1e120039b8
Shawn Clisby <shawn.clisby@gmail.com>
2020-10-28 21:04:12 -0400

    Added/Tested fake Components with React Routing, imported materialize-css library, create Redux store.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "./Header";
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>Survey New</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/surveys/new" component={SurveyNew} />
+        <Route path="/surveys" component={Dashboard} />
+        <Route path="/" component={Landing} />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
